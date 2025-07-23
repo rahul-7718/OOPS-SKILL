5a77fb7dff0bcc9bb2705362bc59fa01dbef7fa8
rahul-7718 <2420080025@klh.edu.in>
2025-07-23 11:30:24 +0530

    Add files via upload

---
+++ TypeConversion.java
@@ -0,0 +1,13 @@
+package oops;
+
+public class TypeConversion {
+
+
+    public static void main(String[] args) {
+        int n = 100;
+        double d = n; // int to double (implicit conversion)
+
+        System.out.println("Integer value: " + n);
+        System.out.println("Converted to double: " + d);
+    }
+}
